c350143a6dc101680a2b5909b41d4d9810b17828
Johan Pimentel <johan.pm47@gmail.com>
2016-02-01 11:49:56 -0400

    GeoQuiz Challenge app

---
+++ GeoQuiz Challenge/app/src/main/java/com/pm47gmail/johan/abapp1/Question.java
@@ -0,0 +1,28 @@
+package com.pm47gmail.johan.abapp1;
+
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+}
